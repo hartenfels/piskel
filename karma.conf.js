7ceb1a1e3daa131e937725e8bb623c8fa98db9c5
Carsten Hartenfels <carsten.hartenfels@gmail.com>
2020-10-13 00:42:09 +0200

    Fix up promise-polyfill path in karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: piskelScripts.concat([
-      './node_modules/promise-polyfill/promise.js'
+      './node_modules/promise-polyfill/dist/polyfill.js'
     ]),
 
 
